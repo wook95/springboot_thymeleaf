8bee026429f20ac78b1cd40e0a59e4dd56a51920
wook95 <80494742+wook95@users.noreply.github.com>
2021-05-14 11:53:09 +0900

    init

---
+++ Springboot_Mybatis_thymeleaf/src/main/java/com/iu/ss1/aop/transfer/Card.java
@@ -0,0 +1,13 @@
+package com.iu.ss1.aop.transfer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Card {
+
+	
+	public void checkCard() {
+		System.out.println("카드 태");
+	}
+	
+}
---
+++ Springboot_Mybatis_thymeleaf/src/test/java/com/iu/ss1/aop/transfer/TransferTest.java
@@ -0,0 +1,26 @@
+package com.iu.ss1.aop.transfer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferTest {
+
+	@Autowired
+	private Transfer transfer;
+	@Autowired
+	private Card card;
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	
+	
+	
+	
+}

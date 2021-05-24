992e04f22e01aa5d1f7be1da071621e9d6040ae3
wook95 <80494742+wook95@users.noreply.github.com>
2021-05-24 09:15:48 +0900

    메세지기능 fix

--- Springboot_Mybatis_thymeleaf/src/main/java/com/iu/ss1/config/MessageLocaleConfig.java
+++ Springboot_Mybatis_thymeleaf/src/main/java/com/iu/ss1/config/MessageLocaleConfig.java
@@ -5,63 +5,47 @@ import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-
+@EnableWebMvc
 @Configuration
 public class MessageLocaleConfig implements WebMvcConfigurer{
 
-	
 	@Bean
-	public LocaleResolver localResolver() {
-		//session을 사용하여 설정하는 경우
+	public LocaleResolver localeResolver() {
+		//session을 사용하여 설정
 		SessionLocaleResolver resolver = new SessionLocaleResolver();
 		resolver.setDefaultLocale(Locale.KOREAN);
-	
 		
-		//쿠키를 사용하여 설정
-		CookieLocaleResolver cookiResolver = new CookieLocaleResolver();
-		cookiResolver.setDefaultLocale(Locale.KOREAN);
-		cookiResolver.setCookieName("lang");
+		//cookie를 사용사여 설정
+		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+		cookieLocaleResolver.setDefaultLocale(Locale.KOREAN);
+		cookieLocaleResolver.setCookieName("lang");
 		
-		return cookiResolver;
-		
-		
-		
+		return cookieLocaleResolver;
 	}
 	
-	
-	//interceptor
-	
-	
+	//Interceptor
+	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
 		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
 		localeChangeInterceptor.setParamName("lang");
-		//파라미터에 따라 언어 구분
+		//parameter를 받아서 언어 구분
 		//url?lang=en
-		
-		
 		return localeChangeInterceptor;
-		
-		
 	}
-
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-
 		registry.addInterceptor(localeChangeInterceptor())
-		.addPathPatterns("/**");
-		
-		
-		// TODO Auto-generated method stub
+				.addPathPatterns("/**");
+	
 	}
-	
-	
-	
-	
 	
 }
---
+++ Springboot_Mybatis_thymeleaf/src/main/java/com/iu/ss1/schedule/TestSchedule.java
@@ -0,0 +1,22 @@
+package com.iu.ss1.schedule;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestSchedule {
+	
+//	@Scheduled(fixedRateString = "1000", initialDelayString = "2000")
+//	public void fixRateScheduleTest()throws Exception{
+//	
+//		
+//		System.out.println("fixRateSchedule");
+//	}
+//	
+//	@Scheduled(cron = "* * * * * *")
+//	public void cronTest()throws Exception{
+//		System.out.println("cron test");
+//	}
+//	
+	
+}
